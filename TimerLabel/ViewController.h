b12028d205b338373471588a147c4523ba11d169
Baby518 <ZhangCharles518@gmail.com>
2015-01-25 21:25:41 +0800

    Initial Commit

---
+++ TimerLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimerLabel
+//
+//  Created by zhangchao on 15/1/25.
+//  Copyright (c) 2015年 zhangchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
